4192fe3df814c4cee0bdb82fe50be701c3b22117
Jeff Davenport <jeffreydvp@gmail.com>
2018-11-22 16:42:28 -0700

    Update comment and version

--- src/cryngine.cr
+++ src/cryngine.cr
@@ -1,6 +1,4 @@
-# TODO: Write documentation for `Cryngine`
+# An advanced 3D game engine in development with the goal in mind of convention, speed, and high quality over configuration
 module Cryngine
-  VERSION = "0.1.0"
-
-  # TODO: Put your code here
+  VERSION = "0.0.1"
 end
