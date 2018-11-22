52a2eb9bd9591d1a08f586f0bf83f6ea38144f62
Jeff Davenport <jeffreydvp@gmail.com>
2018-11-22 16:49:49 -0700

    Add in server

--- src/cryngine.cr
+++ src/cryngine.cr
@@ -1,4 +1,23 @@
 # An advanced 3D game engine in development with the goal in mind of convention, speed, and high quality over configuration
+require 'cryngine/*'
+require 'cryngine/devices/*'
+require 'cryngine/display/*'
+require 'cryngine/system/*'
+
 module Cryngine
   VERSION = "0.0.1"
+  Log     = System::Log
+  alias Loop = System::Loop
+
+  # module Display
+  # end
+
+  # module Devices
+  # end
+
+  # module System
+  # end
+
+  # module Server
+  # end
 end
---
+++ src/cryngine/server/authentication.cr
@@ -0,0 +1,4 @@
+module Cryngine
+  struct Authentication
+  end
+end
